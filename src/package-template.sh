5f1050f9671d1f3f2009bdd2b484c35b1012345d
sakaki91 <sakaki@tuta.io>
2026-07-30 09:33:31 -0300

    Update - Recipe Template.
    modified:
      pkg/package-template.sh

--- src/package-template.sh
+++ src/package-template.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-NAME=""
-VERSION=""
-DESCRIPTION=""
-SOURCE=""
-DEPENDENCIES=()
+NAME="package"
+VERSION="1.0"
+DESCRIPTION="package description."
+SOURCE="https://package-link.com" # or git repository
+DEPENDENCIES=(
+    "dependencie1"
+    "dependencia2"
+)
 
 function install(){
-	# [...]
+	# [...] Workarround to install.
 }
 
 function remove(){
-    # [...]
+    # [...] Workarround to remove.
 }
