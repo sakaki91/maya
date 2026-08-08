bfbdd10757159dab1e20a7b2728935d1c720db5f
sakaki91 <sakaki@tuta.io>
2026-08-08 00:33:07 -0300

    Update 2.2

--- install.sh
+++ install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-[[ $EUID != 0 ]] && printf "\e[0;91merror:\e[0m you can't run this without root access.\n" && exit
+PATH_MAYA_BIN="/usr/local/bin"
+PATH_MAYA_TREE="/usr/local/share/maya"
+PATH_MAYA_REGISTRY="$MAYA_TREE/pkg/.info"
+PATH_MAYA_DOC="$MAYA_TREE/doc"
 
-MAYA_BIN="/usr/local/bin"
-MAYA_TREE="/usr/local/share/maya"
-MAYA_REGISTRY="$MAYA_TREE/pkg/.info"
-MAYA_DOC="$MAYA_TREE/doc"
-  
-if [[ $1 == "--remove" ]]; then
-    [[ ! -f "$MAYA_BIN"/maya && ! -d $MAYA_TREE ]] && printf "error\n" && exit
-    rm -f $MAYA_BIN/maya
-    rm -rf $MAYA_TREE
-    exit
+if [[ $EUID == 0 ]]; then
+    if [[ $1 == "--remove" ]]; then
+        if [[ ! -f "$PATH_MAYA_BIN"/maya && ! -d $PATH_MAYA_TREE ]]; then
+            printf "\033[0;91merror:\033[0m maya is not installed.\n"
+        else
+            rm -f $PATH_MAYA_BIN/maya
+            rm -rf $PATH_MAYA_TREE
+        fi
+    else
+        mkdir -p $PATH_MAYA_TREE $PATH_MAYA_REGISTRY $PATH_MAYA_DOC
+        /usr/bin/install maya $PATH_MAYA_BIN
+        /usr/bin/install {README,LICENSE} $PATH_MAYA_DOC
+        cp -r {pkg,src} $PATH_MAYA_TREE
+    fi
 else
-    mkdir -p $MAYA_TREE $MAYA_REGISTRY $MAYA_DOC
-    /usr/bin/install maya $MAYA_BIN
-    /usr/bin/install {README,LICENSE} $MAYA_DOC
-    cp -r {pkg,src} $MAYA_TREE
+    printf "\033[0;91merror:\033[0m you can't run without root access.\n"
 fi
